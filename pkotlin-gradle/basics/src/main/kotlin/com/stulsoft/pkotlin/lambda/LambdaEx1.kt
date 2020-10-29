8cce31ce0a7afcbaa449465a637232d0c4f24e80
Yuriy Stul <yuriy.s@webpals.com>
2020-10-29 13:54:38 +0200

    Exercises with lambda

--- pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/lambda/LambdaEx1.kt
+++ pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/lambda/LambdaEx1.kt
@@ -10,32 +10,37 @@ import org.slf4j.LoggerFactory
 /**
  * @author Yuriy Stul
  */
+object LambdaEx1 {
+    private val logger: Logger = LoggerFactory.getLogger("")
 
-val logger: Logger = LoggerFactory.getLogger("")
+    /**
+     * Function with body block. Must use return operator
+     */
+    private fun test1(f: (String) -> String): String {
+        return f("test1")
+    }
 
-fun main() {
-    logger.info("==>main")
-    logger.info("test1: {}", test1 { s -> "$s processed 1" })
-    logger.info("test1: {}", test1 { "$it processed 2" })
-    logger.info("test2: {}", test2 { "$it processed" })
+    /**
+     * Function without body block. Works without return operator
+     */
+    private fun test2(f: (String) -> String) = f("test2")
+
+    /**
+     * Function without body block. Works without return operator
+     */
+    private fun test3(f: (String) -> String) = run {
+        logger.info("In test 3")
+        f("test3")
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        logger.info("==>main")
+        logger.info("test1: {}", test1 { s -> "$s processed 1" })
+        logger.info("test1: {}", test1 { "$it processed 2" })
+        logger.info("test2: {}", test2 { "$it processed" })
+        logger.info("test3: {}", test3 { text -> "$text done for test3" })
+    }
 }
 
-/**
- * Function with body block. Must use return operator
- */
-fun test1(f: (String) -> String): String {
-    return f("test1")
-}
 
-/**
- * Function without body block. Works without return operator
- */
-fun test2(f: (String) -> String) = f("test2")
-
-/**
- * Function without body block. Works without return operator
- */
-fun test3(f: (String) -> String) = run {
-    logger.info("In test 3")
-    f("test2")
-}
---
+++ pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/lambda/LambdaEx2.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020. Yuriy Stul
+ */
+
+package com.stulsoft.pkotlin.lambda
+
+/**
+ * @author Yuriy Stul
+ */
+object LambdaEx2 {
+    private fun f1(k: Int, f: (Int) -> Int): Int {
+        return f(k)
+    }
+
+    private fun foo(p1: Int, f1: (Int) -> Int, f2: (Int) -> Int): Int {
+        return f1(f2(p1))
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("${f1(123) { it + 22 }}")
+        println("${f1(15) { it + 22 }}")
+
+        val r3 = f1(3) { i -> i + 4 }
+        println("r3=$r3")
+
+        val r4 = foo(123, { it + 2 }, { it * 50 })
+        println("r4=$r4")
+        println("${foo(321, { it + 2 }, { it * 50 })}")
+
+        val r5 = foo(76,
+                {
+                    println("1 it=$it")
+                    it * 54
+                },
+                {
+                    println("2 it=$it")
+                    it - 17
+                })
+        println("r5=$r5")
+    }
+}
