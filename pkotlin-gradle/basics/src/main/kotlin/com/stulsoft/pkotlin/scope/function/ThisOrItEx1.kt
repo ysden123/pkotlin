f790d1776291345a354ca1bae2f51a7f951cc630
Yuriy Stul <ysden123@gmail.com>
2020-10-30 12:46:11 +0200

    Scope functions, continue part I

--- pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/scope/function/ThisOrItEx1.kt
+++ pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/scope/function/ThisOrItEx1.kt
@@ -1,7 +1,32 @@
+/*
+ * Copyright (c) 2020. Yuriy Stul
+ */
+
 package com.stulsoft.pkotlin.scope.function
 
 /**
  * @author Yuriy Stul
  */
 object ThisOrItEx1 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        test1()
+    }
+
+    private fun test1() {
+        println("==>test1")
+        val str = "Hello"
+        // this
+        // lambda receiver
+        str.run {
+            println("The receiver string length: $length")
+            //println("The receiver string length: ${this.length}") // does the same
+        }
+
+        // it
+        // lambda argument
+        str.let {
+            println("The receiver string's length is ${it.length}")
+        }
+    }
 }
