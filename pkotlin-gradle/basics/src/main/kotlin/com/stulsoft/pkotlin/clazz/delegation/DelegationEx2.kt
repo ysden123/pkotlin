22485eead5a59c460294b3d937e08cf3810be7f1
Yuriy Stul <ysden123@gmail.com>
2020-10-31 12:52:15 +0200

    reformatting

--- pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/clazz/delegation/DelegationEx2.kt
+++ pkotlin-gradle/basics/src/main/kotlin/com/stulsoft/pkotlin/clazz/delegation/DelegationEx2.kt
@@ -9,16 +9,16 @@ Simple delegation example. Call function from delegated interface.
  */
 
 
-interface SomeInterface2{
-    fun foo():String
+interface SomeInterface2 {
+    fun foo(): String
 }
 
-class SomeInterface2Impl(val name:String) : SomeInterface2{
+class SomeInterface2Impl(val name: String) : SomeInterface2 {
     override fun foo() = "$name handled"
 }
 
-class SomeClassWithDelegation2(private val si:SomeInterface2): SomeInterface2 by si{
-    fun bar(){
+class SomeClassWithDelegation2(private val si: SomeInterface2) : SomeInterface2 by si {
+    fun bar() {
         println("In bar. foo: ${foo()}")
     }
 }
